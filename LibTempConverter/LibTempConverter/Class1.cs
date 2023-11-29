5dea6522a205fff6f135de2d8e253207ff72c5c2
Your Name <you@example.com>
2023-11-29 11:14:14 +0200

    3 lab

---
+++ LibTempConverter/LibTempConverter/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace TemperatureConverterLibrary
+{
+    public class TemperatureConverter
+    {
+        public double CelsiusToFahrenheit(double celsius)
+        {
+            return (celsius * 9 / 5) + 32;
+        }
+    }
+}
---
+++ block_3/block_3/main.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Reflection;
+class Program
+{
+    static void Main()
+    {
+        string dllFileName = "LibTempConverter.dll";
+
+        Assembly assembly = Assembly.LoadFrom(dllFileName);
+
+        object converterInstance = Activator.CreateInstance(assembly.GetType("TemperatureConverterLibrary.TemperatureConverter"));
+
+        Console.Write("Temperature in Celsius: ");
+        double celsiusValue;
+        if (!double.TryParse(Console.ReadLine(), out celsiusValue))
+        {
+            Console.WriteLine("Incorrect answer. Be kind, enter a numeric value.");
+            return;
+        }
+
+        // Виклик методу CelsiusToFahrenheit за допомогою рефлексії
+        MethodInfo methodInfo = converterInstance.GetType().GetMethod("CelsiusToFahrenheit");
+        double fahrenheitValue = (double)methodInfo.Invoke(converterInstance, new object[] { celsiusValue });
+
+        // Виведення результату
+        Console.WriteLine($"{celsiusValue} degrees Celsius = {fahrenheitValue} degrees Fahrenheit");
+
+        Console.ReadLine();
+    }
+}
